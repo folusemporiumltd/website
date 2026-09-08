b727756d3fb0a9a0b7124387db1903328f62aaef
folusemporiumltd <adeoyemofoluwake@gmail.com>
2026-09-08 21:50:12 +0100

    Configure Folus Emporium GA4 measurement ID

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -1,2 +1,17 @@
 import Script from 'next/script'
-export default function GoogleAnalytics(){const id=process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;if(!id)return null;return <><Script src={`https://www.googletagmanager.com/gtag/js?id=${id}`} strategy="afterInteractive"/><Script id="google-analytics" strategy="afterInteractive">{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','${id}',{anonymize_ip:true});`}</Script></>}
+
+const DEFAULT_GA_MEASUREMENT_ID = 'G-43RCBFF7VK'
+
+export default function GoogleAnalytics() {
+  const id = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || DEFAULT_GA_MEASUREMENT_ID
+
+  return <>
+    <Script src={`https://www.googletagmanager.com/gtag/js?id=${id}`} strategy="afterInteractive" />
+    <Script id="google-analytics" strategy="afterInteractive">{`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${id}', { anonymize_ip: true });
+    `}</Script>
+  </>
+}
